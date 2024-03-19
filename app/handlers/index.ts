bcb568989d35806a1ee02385ffb5901ef2851ffe
Todd Baert <todd.baert@dynatrace.com>
2024-03-19 18:42:24 -0400

    chore: cleanup
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- app/handlers/index.ts
+++ app/handlers/index.ts
@@ -1,85 +1,18 @@
 import { OpenFeature } from '@openfeature/server-sdk';
+import { parseContext } from './util';
 import { Request, Response } from 'express';
-
-let targetingKeys = new Set<string>;
-
-setInterval(() => {
-  console.log(`unique users: ${targetingKeys.size}`);
-}, 5000);
-
-type AppSpecificContext = {
-  /**
-   * Value of the user User-Agent header
-   */
-  userAgent?: string;
-  /**
-   * Value of window.outerHeight
-   */
-  windowHeight?: number;
-  /**
-   * Value of window.outerWidth
-   */
-  windowWidth?: number;
-  /**
-   * Value of battery.level * 100
-   */
-  batteryPercentage?: number,
-  /**
-   * Value of "navigator.language"
-   */
-  language?: string
-  /**
-   * Value of navigator.connection.effectiveType
-   * Possible values: "slow-2g", "2g", "3g", or "4g"
-   */
-  connectionType?: string
-  /**
-   * A string logically identifying the subject of evaluation (end-user, service, etc).
-   * https://openfeature.dev/specification/glossary#targeting-key
-   */
-  targetingKey?: string
-};
 
 export const indexHandler = async (req: Request, res: Response) => {
 
   const client = OpenFeature.getClient();
-  const context = generateContext(req);
+  const context = parseContext(req);
   const hexColor = await client.getStringValue('hex-color', '000', context);
-  const emoji = await client.getStringValue('emoji', "", context );
-
-  if (process.env.DEBUG == 'true') {
-    console.log(context);
-  }
+  const emoji = await client.getStringValue('emoji', "", context);
 
   res.render("index", { title: "Demo", hexColor, emoji });
-
-  if (context.targetingKey && !targetingKeys.has(context.targetingKey)) {
-    targetingKeys.add(context.targetingKey);
-  }
 };
 
 export const errorHandler = (err: any, req: Request, res: Response) => {
   console.error(err);
   res.render("index", { title: "Error", hexColor: '000', emoji: '⚠️' });
 };
-
-// enrich the context
-const generateContext = (req: Request): AppSpecificContext => {
-  return {
-    userAgent: req.headers['user-agent'],
-    ...parseContextParam(req.query.context)
-  };
-};
-
-// parse the context sent from the client
-const parseContextParam = (param: Request['query'][keyof Request['query']]): AppSpecificContext => {
-  let context = {};
-  try {
-    if (typeof param === 'string') {
-      context = JSON.parse(decodeURIComponent(param));
-    }
-  } catch (e) {
-    console.log('invalid context');
-  }
-  return context;
-}
